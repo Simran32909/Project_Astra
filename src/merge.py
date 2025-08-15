8c14c174e60d2b39fb196b70fbdccf9096ff3d91
Simran32909 <m.simx2909@gmail.com>
2025-08-15 17:14:35 +0000

    Implement script to merge base model and PEFT adapter

--- src/merge.py
+++ src/merge.py
@@ -6,4 +6,48 @@ from omegaconf import DictConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
 
-@hydra.main(config_path="configs", config_name="config.yaml")
+@hydra.main(config_path="../configs", config_name="config.yaml")
+def merge(cfg:DictConfig):
+    base_model_id=cfg.model.id
+    
+    adapter_path=os.path.join(cfg.trainer.output_dir, "final_adapter")
+    merged_model_path=os.path.join(cfg.trainer.output_dir, "merged_model")
+    
+    print(f"Loading base model from {base_model_id}")
+    base_model = AutoModelForCausalLM.from_pretrained(
+        base_model_id,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+        trust_remote_code=True
+        )
+    
+    print(f"Loading tokenizer from {base_model_id}")
+    tokenizer = AutoTokenizer.from_pretrained(
+        base_model_id,
+        trust_remote_code=True
+    )
+
+    print(f"Loading PEFT adapter from {adapter_path}...")
+    if not os.path.exists(adapter_path):
+        raise FileNotFoundError(f"Adapter path {adapter_path} does not exist")
+    
+    model = PeftModel.from_pretrained(
+        base_model,
+        adapter_path
+    )
+    
+    print(f"Merging PEFT adapter with base model...")
+    model=model.merge_and_unload()
+
+    print(f"Saving merged model to {merged_model_path}...")
+    os.makedirs(
+        merged_model_path, 
+        exist_ok=True
+    )
+    model.save_pretrained(merged_model_path)
+    tokenizer.save_pretrained(merged_model_path)
+    
+    print(f"Model saved to {merged_model_path}")
+
+if __name__ == "__main__":
+    merge()
